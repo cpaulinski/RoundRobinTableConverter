9170981f56c4e6605ff0c8d37b60341e66ac1d35
Chad Paulinski <cpaulinski89@gmail.com>
2015-11-27 01:29:13 -0500

    Initial Commit++. Added base project

---
+++ RoundRobinTableConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+    This tool is to retrieve the Round Robin Table from Google Sheets,
+    convert the data into a more readable format on a different sheet,
+    and perform calculations to sort winners based on tiebreakers
+*/
+namespace RoundRobinTableConverter
+{
+    class RoundRobinTableConverter
+    {
+        static void Main(string[] args)
+        {
+            //TODO: add RESTful service to retrieve and post google sheets information
+            
+            //TODO: add project to perform calculations to the data:
+            //  1. Put Win-Loss in correct columns
+            //  2. Add data for sorting tiebreakers
+        }
+    }
+}
